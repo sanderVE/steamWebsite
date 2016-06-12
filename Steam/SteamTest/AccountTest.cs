2cedf8b528f80c07164011647b870d13faf3ccb4
sanderVE <sandervanelderen@gmail.com>
2016-06-12 23:43:41 +0200

    Unittest toegevoegd

---
+++ Steam/SteamTest/AccountTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Steam.Models;
+
+namespace SteamTest
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void Accounttest()
+        {
+            AccountModel am = new AccountModel("username", "password", "email", 1);
+            Assert.AreEqual("username", am.name);
+            Assert.AreEqual("password", am.password);
+            Assert.AreEqual("email", am.email);
+            Assert.AreEqual(1, am.id);
+        }
+    }
+}
---
+++ Steam/SteamTest/GameTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Steam.Models;
+
+namespace SteamTest
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void Gametest()
+        {
+            GameModel gm = new GameModel(1, "game", 60, Convert.ToDateTime("5-5-2016"));
+            Assert.AreEqual(1, gm.id);
+            Assert.AreEqual("game", gm.title);
+            Assert.AreEqual(60, gm.prijs);
+            Assert.AreEqual("5-5-2016", gm.releaseDate);
+        }
+    }
+}
